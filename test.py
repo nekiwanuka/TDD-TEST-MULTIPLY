2086bc08d7e9324858290cfa490c2d1e98fe880a
NEKIWANUKA <nekiwanuka@gmail.com>
2023-06-25 12:48:34 +0300

    Code formatted with black

--- test.py
+++ test.py
@@ -2,16 +2,20 @@ from multiply import multiply
 
 
 def test_multiply():
-    assert multiply(1, 1) ==  1
-    
+    assert multiply(1, 1) == 1
+
+
 def test_multiply_2():
     assert multiply(2, 2) == 4
 
+
 def test_multiply_3():
-    assert multiply(3, 3) == 9 
-    
+    assert multiply(3, 3) == 9
+
+
 def test_multiply_4():
     assert multiply(4, 4) == 16
-    
+
+
 def test_multiply_5():
     assert multiply(23, 45) == 23 * 45
